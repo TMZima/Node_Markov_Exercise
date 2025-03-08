cf471160e46e816d797288a0af557c5a7bf242d6
Trey Zima <trey.m.zima@gmail.com>
2025-03-08 15:20:54 -0700

    Completed Step 1

--- node-markov/markov.js
+++ node-markov/markov.js
@@ -1,13 +1,11 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
-
   /** build markov machine; read in text.*/
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
+    this.words = words.filter((c) => c !== "");
     this.makeChains();
   }
 
@@ -17,13 +15,41 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    this.chains = {};
+
+    for (let i = 0; i < this.words.length; i++) {
+      let word = this.words[i];
+      let nextWord = this.words[i + 1] || null;
+
+      if (!this.chains[word]) {
+        this.chains[word] = [];
+      }
+      this.chains[word].push(nextWord);
+    }
   }
 
+  // select an element from an array
+
+  static pickElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    let keys = Object.keys(this.chains);
+    let key = MarkovMachine.pickElement(keys);
+    let output = [];
+
+    while (output.length < numWords && key !== null) {
+      output.push(key);
+      key = MarkovMachine.pickElement(this.chains[key]);
+    }
+
+    return output.join(" ");
   }
 }
+
+module.exports = {
+  MarkovMachine,
+};
